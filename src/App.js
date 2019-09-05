5838e119f49a9fc898fb4aaaab58439dd66a1e5d
David N <bigair_1080@hotmail.com>
2019-09-05 18:20:40 -0300

    Added Static NavHeader component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './containers/HeaderNav/HeaderNav.scss';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+
 
 function App() {
   return (
-    <div>
-      Project Start!
-    </div>
+    <HeaderNav />
   );
 }
 
