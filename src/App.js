53ef5764a9109a7da55f66b0c45f82fcee385a81
David N <bigair_1080@hotmail.com>
2019-09-07 15:30:55 -0300

    finished off sidebar nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './containers/HeaderNav/HeaderNav.scss';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
+import SideBar from './containers/SideBar/SideBar';
 
 
 function App() {
   return (
-    <HeaderNav />
+    <React.Fragment>
+      <HeaderNav />
+      <SideBar />
+    </React.Fragment>
   );
 }
 
---
+++ src/containers/SideBar/SideBarItem/SideBarItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Icon, Menu } from 'semantic-ui-react';
+import './SideBarItem.scss';
+
+const SideBarItem = ({ highlight, icon, label }) => {
+  const highLight = highlight ? 'highlight-item' : null;
+  return (
+    <Menu.Item className={['sidebar-item', highLight].join(' ')}>
+      <div className='sidebar-item-alignment-container'>
+        <span>
+          <Icon size='large' name={icon} />
+        </span>
+        <span>{label}</span>
+      </div>
+    </Menu.Item>
+  );
+}
+
+export default SideBarItem;
