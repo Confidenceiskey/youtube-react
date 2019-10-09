2e99ec868651af8354770a62ccb94e93f248e321
David N <bigair_1080@hotmail.com>
2019-10-09 20:58:51 -0300

    updated watch & video with redux store

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './Watch.scss';
 import Comments from '../Comments/Comments';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 import Video from '../../components/Video/Video';
 import VideoInfoBox from '../../components/VideoInfoBox/VideoInfoBox';
 import VideoMetadata from '../../components/VideoMetadata/VideoMetadata';
+import * as watchActions from '../../store/actions/watch';
+import { getYoutubeLibraryLoaded } from '../../store/reducers/api';
 
 class Watch extends Component {
 
   getVideoId() {
     const searchParams = new URLSearchParams(this.props.location.search);
     return searchParams.get('v');
+  }
+
+  componentDidMount() {
+    if (this.props.youtubeLibraryLoaded) {
+      this.fetchWatchContent();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.youtubeLibraryLoaded !== prevProps.youtubeLibraryLoaded) {
+      this.fetchWatchContent();
+    }
+  }
+
+  fetchWatchContent() {
+    const videoId = this.getVideoId();
+    if (!videoId) {
+      this.props.history.push('/');
+    }
+    this.props.fetchWatchDetails(videoId, this.props.channelId);
   }
 
   render() {
@@ -26,4 +51,15 @@ class Watch extends Component {
   }
 }
 
-export default Watch;
+function mapStateToProps(state) {
+  return {
+    youtubeLibraryLoaded: getYoutubeLibraryLoaded(state)
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  const fetchWatchDetails = watchActions.details.request;
+  return bindActionCreators({fetchWatchDetails}, dispatch);
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Watch));
